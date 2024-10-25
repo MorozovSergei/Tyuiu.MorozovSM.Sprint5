7f89a767df318d2934d83c8ef121c0617cf408ea
Сергей Морозов <Tyuiu.MorozovMS@outlook.com>
2024-10-25 19:13:55 +0500

    Task4

--- Tyuiu.MorozovSM.Sprint5.Task4.V8.Lib/DataService.cs
+++ Tyuiu.MorozovSM.Sprint5.Task4.V8.Lib/DataService.cs
@@ -3,12 +3,12 @@ using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.MorozovSM.Sprint5.Task4.V8.Lib
 {
-    public class DataService : ISprint5Task5V8
+    public class DataService : ISprint5Task4V8
     {
         public double LoadFromDataFile(string path)
         {
             double x = Convert.ToDouble(File.ReadAllText(path));
-            double y = Math.Round((Math.Pow(x,2)/Math.Sin(x))+3, 3);
+            double y = Math.Round((Math.Pow(x, 2) / Math.Sin(x)) + 3, 3);
             return y;
         }
     }
--- Tyuiu.MorozovSM.Sprint5.Task4.V8.Test/DataServiceTest.cs
+++ Tyuiu.MorozovSM.Sprint5.Task4.V8.Test/DataServiceTest.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.MorozovSM.Sprint5.Task4.V8.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\DataSprint5\InPutDataFileTask4V0.txt";
+            string path = @"C:\DataSprint5\InPutDataFileTask4V8.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool Exists = fileInfo.Exists;
             Assert.IsTrue(Exists);
